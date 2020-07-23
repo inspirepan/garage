2b12bb70a7b84524193aeb9d7708d53e945c0f8e
PanJixiang <narfnas@163.com>
2020-07-23 19:27:36 +0800

    97 交错字符串

--- src/Algorithm/S97.java
+++ src/Algorithm/S97.java
@@ -9,7 +9,7 @@ public class S97 {
     public boolean isInterleave(String s1, String s2, String s3) {
         int m = s1.length(), n = s2.length();
         if (s3.length() != m + n) return false;
-        // 动态规划，dp[i,j]表示s1前i字符能与s2前j字符组成s3前i+j个字符；
+        // 动态规划，dp[i,j]表示s1前i字符能与s2前j字符组成s3前i+j个字符
         boolean[][] dp = new boolean[m + 1][n + 1];
         dp[0][0] = true;
         for (int i = 1; i <= m && s1.charAt(i - 1) == s3.charAt(i - 1); i++) dp[i][0] = true; // 不相符直接终止
