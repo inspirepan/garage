713ad0606c3d48d37baad1a06a6251f07f34d43a
PanJixiang <narfnas@163.com>
2020-07-27 17:43:53 +0800

    判断二叉树是否对称

---
+++ src/Algorithm/S101.java
@@ -0,0 +1,24 @@
+package Algorithm;
+
+import DataStructure.TreeNode;
+
+/* 判断二叉树是否对称
+*  递归 */
+public class S101 {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return true;
+        return isSymmetric(root.left, root.right);
+    }
+
+    public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null)
+            return true;
+        else if (left == null || right == null)
+            return false;
+        else if (left.val != right.val)
+            return false;
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+
+}
