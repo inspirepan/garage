a61600df0ad867b2ce804d9fea3594cec89624ac
PanJixiang <narfnas@163.com>
2020-07-27 17:16:09 +0800

    二叉树判等

---
+++ src/Algorithm/S100.java
@@ -0,0 +1,16 @@
+package Algorithm;
+
+import DataStructure.TreeNode;
+
+/* 比较两棵二叉树是否相等 */
+public class S100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        else if (p == null || q == null)
+            return false;
+        else if (p.val != q.val)
+            return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
