0a19d260a71ad95928efa4560896d2f42a0f0a41
PanJixiang <narfnas@163.com>
2020-07-29 00:17:48 +0800

    110

--- src/test.java
+++ src/test.java
@@ -6,9 +6,10 @@ import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-        S102 s = new S102();
-        TreeNode root = new TreeNode(-8, new TreeNode(-6), new TreeNode(7));
-        System.out.println(s.levelOrder(root));
+        S105 s = new S105();
+        int[] pre = new int[] { 3, 9, 20, 15, 7 };
+        int[] in = new int[] { 9, 3, 15, 20, 7 };
+        TreeNode root = s.buildTree(pre, in);
     }
 
     /* 输出二维数组 */
