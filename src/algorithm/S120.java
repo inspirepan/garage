8befdf80dd2dc6eef39d5434b8b015a85586e391
PanJixiang <narfnas@163.com>
2020-08-02 16:11:56 +0800

    120

--- src/algorithm/S120.java
+++ src/algorithm/S120.java
@@ -11,10 +11,10 @@ public class S120 {
         // 加1可以不用初始化最后一层
         int[][] dp = new int[triangle.size()+1][triangle.size()+1];
 
-        for (int i = triangle.size()-1; i>=0; i--){
+        for (int i = triangle.size() - 1; i >= 0; i--) {
             List<Integer> curTr = triangle.get(i);
-            for(int j = 0 ; j< curTr.size(); j++){
-                dp[i][j] = Math.min(dp[i+1][j], dp[i+1][j+1]) + curTr.get(j);
+            for (int j = 0; j < curTr.size(); j++) {
+                dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1]) + curTr.get(j);
             }
         }
         return dp[0][0];
