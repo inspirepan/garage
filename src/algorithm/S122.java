0fb48ff85ed44ea9418e00c98bd29fd2413e8b29
PanJixiang <narfnas@163.com>
2020-08-02 16:59:54 +0800

    122

--- src/algorithm/S122.java
+++ src/algorithm/S122.java
@@ -1,5 +1,19 @@
 package algorithm;
 
 public class S122 {
-    
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0) {
+            return 0;
+        }
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int currentPrice = prices[i];
+            if (currentPrice > minPrice) {
+                maxProfit += currentPrice - minPrice;
+            }
+            minPrice = currentPrice;
+        }
+        return maxProfit;
+    }
 }
