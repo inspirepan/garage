7ad386bf0c6e969db7f40af6d29d2d877f3088f2
PanJixiang <narfnas@163.com>
2020-08-03 19:23:58 +0800

    127

--- src/main/java/algorithm/S127.java
+++ src/main/java/algorithm/S127.java
@@ -1,12 +1,13 @@
 package algorithm;
 
-import java.util.*;
-
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.*;
 
 public class S127 {
     /*
-     * 完完全全参考的官方题解，
+     * 完全参考的官方题解，
      * 虽然是个无向图的广度优先遍历，但是 Queue，Pair，Map这些数据结构的调用实在还是不熟练
      */
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
@@ -21,8 +22,28 @@ public class S127 {
             }
         });
         System.out.println(allComboDict);
-        Queue<Pair<String, Integer>> queue = new LinkedList<>();
+        Queue<Pair<String, Integer>> Q = new LinkedList<>();
+        Q.add(new ImmutablePair<>(beginWord, 1));
+        Map<String, Boolean> visited = new HashMap<>();
+        visited.put(beginWord, true);
 
+        while (!Q.isEmpty()) {
+            Pair<String, Integer> node = Q.remove();
+            String word = node.getKey();
+            int level = node.getValue();
+            for (int i = 0; i < length; i++) {
+                String newWord = word.substring(0, i) + '*' + word.substring(i + 1, length);
+                for (String adjacentWord : allComboDict.getOrDefault(newWord, new ArrayList<>())) {
+                    if (adjacentWord.equals(endWord)) {
+                        return level + 1;
+                    }
+                    if (!visited.containsKey(adjacentWord)) {
+                        visited.put(adjacentWord, true);
+                        Q.add(new ImmutablePair<>(adjacentWord, level + 1));
+                    }
+                }
+            }
+        }
         return 0;
     }
 }
