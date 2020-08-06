b3b329c05fae4493d3d6d9c7841d43af50413d53
PanJx <panjx6@outlook.com>
2020-08-06 19:36:05 +0800

    146

---
+++ src/main/java/algorithm/S146.java
@@ -0,0 +1,42 @@
+package algorithm;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/* LRUCache */
+class S146 {
+
+    private LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
+    private final int capacity;
+
+    public S146(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        if (map.containsKey(key)) {
+            int value = map.get(key);
+            map.remove(key);
+            map.put(key, value);
+            return value;
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            map.remove(key);
+        } else if (map.size() == capacity) {
+            Iterator<Map.Entry<Integer, Integer>> it = map.entrySet().iterator();
+            it.next();
+            it.remove();
+        }
+        map.put(key, value);
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such: LRUCache obj =
+ * new LRUCache(capacity); int param_1 = obj.get(key); obj.put(key,value);
+ */
