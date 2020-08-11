b166083b72795c30b234a6165de16a103af02069
PanJx <panjx6@outlook.com>
2020-08-11 18:00:16 +0800

    153

---
+++ src/main/java/algorithm/S153.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+public class S153 {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = (right - left) / 2 + left;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+}
