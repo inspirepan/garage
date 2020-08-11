b1a1265d903c84153539c16a4117d331f6648906
PanJx <panjx6@outlook.com>
2020-08-11 19:36:01 +0800

    154

---
+++ src/main/java/algorithm/S154.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+public class S154 {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                right--;
+            }// 对时间复杂度的影响很明显
+        }
+        return nums[left];
+    }
+}
