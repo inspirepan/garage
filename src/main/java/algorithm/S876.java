3dae7f5fbd84cef48c1a6483b6df7dcedc593a1e
PanJx <panjx6@outlook.com>
2020-08-14 19:54:40 +0800

    876

---
+++ src/main/java/algorithm/S876.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+import datastructure.ListNode;
+
+public class S876 {
+    public ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode fast = head;
+        ListNode low = head;
+        do {
+            fast = fast.next.next;
+            low = low.next;
+
+        } while (fast != null && fast.next != null);
+        return low;
+    }
+}
