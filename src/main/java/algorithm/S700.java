506f0ff8785ed5bf80b96a6362bf311cfd7736b2
PanJx <panjx6@outlook.com>
2020-08-15 02:21:23 +0800

    700

---
+++ src/main/java/algorithm/S700.java
@@ -0,0 +1,35 @@
+package algorithm;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import datastructure.TreeNode;
+
+public class S700 {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        } else if (root.val == val) {
+            return root;
+        } else if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+
+    /* 迭代 */
+    public TreeNode searchBST2(TreeNode root, int val) {
+        TreeNode node = root;
+        while (node != null) {
+            if (node.val == val) {
+                return node;
+            } else if (node.val > val) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        return null;
+    }
+}
