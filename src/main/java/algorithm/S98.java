e4c5936c50b01f0858e72d1809cb2de147a2a858
PanJx <panjx6@outlook.com>
2020-08-15 02:21:03 +0800

    98

--- src/main/java/algorithm/S98.java
+++ src/main/java/algorithm/S98.java
@@ -1,5 +1,8 @@
 package algorithm;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 import datastructure.TreeNode;
 
 public class S98 {
@@ -17,5 +20,25 @@ public class S98 {
             pre = root.val;
             return isValidBST(root.right);
         }
+    }
+
+    /* 迭代 */
+    public boolean isValidBST2(TreeNode root) {
+        long prev = Long.MIN_VALUE;
+        TreeNode node = root;
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        while (node != null || !queue.isEmpty()) {
+            while (node != null) {
+                queue.push(node);
+                node = node.left;
+            }
+            node = queue.pop();
+            if (node.val <= prev) {
+                return false;
+            }
+            prev = node.val;
+            node = node.right;
+        }
+        return true;
     }
 }
