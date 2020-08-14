dec96112e71f077881e896f0439c2fcd6ae6c4f9
PanJx <panjx6@outlook.com>
2020-08-14 19:11:47 +0800

    206 链表

---
+++ src/main/java/algorithm/S206.java
@@ -0,0 +1,68 @@
+package algorithm;
+
+import datastructure.ListNode;
+
+public class S206 {
+    /** 
+     * 每次操作为
+     * h
+     * 1 -> 2 -> 3 -> 4
+     * =>
+     *      h
+     * 2 -> 1 -> 3 -> 4
+     * =>
+     *           h
+     * 3 -> 2 -> 1 -> 4
+    */
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        while (head.next != null) {
+            ListNode node = head.next;
+            ListNode nxt = node.next;
+            node.next = dummy.next;
+            dummy.next = node;
+            head.next = nxt;
+            print(head);
+        }
+        return dummy.next;
+    }
+    
+    /**
+     * 每次操作为
+     * p    c    n
+     * 1 -> 2 -> 3 -> 4
+     * =>
+     *      p    c    n
+     * 1 <- 2    3 -> 4
+     */
+    public ListNode reverseList2(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        while (curr != null) {
+            ListNode nxt = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nxt;
+            print(curr);
+        }
+        return prev;
+    }
+
+    /* 输出单链表 */
+    public static void print(ListNode head) {
+        ListNode ptr = head;
+        if (ptr == null) {
+            System.out.println("null");
+            return;
+        }
+        while (ptr != null) {
+            System.out.print(ptr.val + " ");
+            ptr = ptr.next;
+        }
+        System.out.println(" ");
+    }
+}
