9e01e95af81cb07959a250231018c4a7219ea216
PanJx <panjx6@outlook.com>
2020-08-20 12:29:04 +0800

    S647

---
+++ src/main/java/algorithm/S647.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+public class S647 {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int count = 0;
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i <= j; i++) {
+                if (s.charAt(j) == s.charAt(i)) {
+                    if (dp[i][j] = i == j || j - i == 1 || dp[i + 1][j - 1]) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
