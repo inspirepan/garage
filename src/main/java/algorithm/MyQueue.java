b32a185739c56c372d91fc44cbf31f15e61f70dc
PanJx <panjx6@outlook.com>
2020-08-20 14:08:52 +0800

    232

--- src/main/java/algorithm/MyQueue.java
+++ src/main/java/algorithm/MyQueue.java
@@ -3,11 +3,7 @@ package algorithm;
 import java.util.Deque;
 import java.util.LinkedList;
 
-public class S232 {
-
-}
-
-class MyQueue {
+public class MyQueue {
 
     Deque<Integer> s1 = new LinkedList<>();
     Deque<Integer> s2 = new LinkedList<>();
@@ -26,42 +22,26 @@ class MyQueue {
      * Removes the element from in front of queue and returns that element.
      */
     public int pop() {
-        int t = 0;
-        do {
-            t = s1.pop();
-            if (!s1.isEmpty()) {
-                s2.push(t);
-            } else {
-                break;
-            }
-        } while (true);
-
-        while (!s2.isEmpty()) {
-            s1.push(s2.pop());
-        }
-        return t;
+        peek();
+        return s2.pop();
     }
 
     /**
      * Get the front element.
      */
     public int peek() {
-        int t = 0;
-        while (!s1.isEmpty()) {
-            t = s1.pop();
-            s2.push(t);
+        if (!s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
         }
-
-        while (!s2.isEmpty()) {
-            s1.push(s2.pop());
-        }
-        return t;
+        return s2.peek();
     }
 
     /**
      * Returns whether the queue is empty.
      */
     public boolean empty() {
-        return this.s1.isEmpty();
+        return s2.isEmpty() && s1.isEmpty();
     }
 }
