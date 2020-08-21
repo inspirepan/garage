7e86f98903735afe42715b96fad9605c26ee4e6d
PanJx <panjx6@outlook.com>
2020-08-21 12:41:38 +0800

    162

---
+++ src/main/java/algorithm/S162.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+public class S162 {
+    public int findpeakelement(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = (right - left) / 2 + left;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
