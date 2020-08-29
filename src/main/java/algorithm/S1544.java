7cf8c6ab68893c69d70ed98c9f811a9e142f3fd3
PanJx <panjx6@outlook.com>
2020-08-29 23:50:54 +0800

    1544

--- src/main/java/algorithm/S1544.java
+++ src/main/java/algorithm/S1544.java
@@ -2,6 +2,17 @@ package algorithm;
 
 public class S1544 {
     public String makeGood(String s) {
-
+        var sb = new StringBuilder(s);
+        int len = -1;
+        while (len != sb.length()) {
+            len = sb.length();
+            for (int i = 0; i < sb.length() - 1; i++) {
+                if (Math.abs(sb.charAt(i) - sb.charAt(i + 1)) == 'a' - 'A') {
+                    sb.delete(i, i + 2);
+                    break;
+                }
+            }
+        }
+        return sb.toString();
     }
 }
