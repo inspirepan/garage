9fa99a31904ca1dcd8f243dce7766d1495e7b5ac
PanJx <panjx6@outlook.com>
2020-08-29 23:14:01 +0800

    209

---
+++ src/test/java/algorithm/S209Test.java
@@ -0,0 +1,12 @@
+package algorithm;
+
+import org.junit.jupiter.api.Test;
+
+class S209Test {
+
+    @Test
+    void minSubArrayLen() {
+
+        System.out.println(S209.minSubArrayLen(100,new int[]{2,3,1,2,3,3}));
+    }
+}
