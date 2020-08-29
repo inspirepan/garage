3d7f5a010956f4a014786ba27042ac12fc5b4303
PanJx <panjx6@outlook.com>
2020-08-29 23:14:35 +0800

    1114

---
+++ src/main/java/algorithm/Foo.java
@@ -0,0 +1,65 @@
+package algorithm;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * S1114
+ */
+class Foo {
+
+    private final ReentrantLock lock = new ReentrantLock();
+    private final Condition cond = lock.newCondition();
+    private int count = 1;
+
+    public Foo() {
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+
+        lock.lock();
+        try {
+            printFirst.run();
+            count = 2;
+            cond.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        lock.lock();
+        try {
+            while (count != 2) {
+                cond.await();
+            }
+            printSecond.run();
+            count = 3;
+            cond.signal();
+
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+
+        // printThird.run() outputs "third". Do not change or remove this line.
+
+        lock.lock();
+        try {
+            while (count != 3) {
+                cond.await();
+            }
+            printThird.run();
+            cond.signalAll();
+
+        } finally {
+            lock.unlock();
+        }
+
+    }
+}
