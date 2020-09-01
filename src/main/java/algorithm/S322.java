0ce5d24e920ba715d6a1d764c2be9c9402940858
PanJx <panjx6@outlook.com>
2020-09-01 16:49:20 +0800

    322

---
+++ src/main/java/algorithm/S322.java
@@ -0,0 +1,28 @@
+package algorithm;
+
+import java.util.Arrays;
+
+public class S322 {
+
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 0;
+
+        int notFound = 99999;
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = notFound;
+            for (int coin : coins) {
+                if (i < coin) {
+                    continue;
+                }
+                dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+            }
+        }
+        System.out.println(Arrays.toString(dp));
+        return (dp[amount] == notFound) ? -1 : dp[amount];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new S322().coinChange(new int[]{1,2}, 3));
+    }
+}
