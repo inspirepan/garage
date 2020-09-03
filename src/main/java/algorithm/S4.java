e00cd9c150cc092f81446c151b3380e6a6e22250
PanJx <panjx6@outlook.com>
2020-09-03 19:40:06 +0800

    4

---
+++ src/main/java/algorithm/S4.java
@@ -0,0 +1,40 @@
+package algorithm;
+
+class S4 {
+    /**
+     * 做不出来
+     * 复制的评论区
+     */
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        // 学习这种写法
+        // 注意left和right这么写是从1开始计数的
+        int left = (m + n + 1) / 2;
+        int right = (m + n + 2) / 2;
+        return (findKth(nums1, 0, nums2, 0, left) + findKth(nums1, 0, nums2, 0, right)) / 2.0;
+    }
+
+    public int findKth(int[] nums1, int i, int[] nums2, int j, int k) {
+        //i: nums1的起始位置 j: nums2的起始位置
+        if (i >= nums1.length) {
+            //nums1已经全部排除
+            return nums2[j + k - 1];
+        }
+        if (j >= nums2.length) {
+            //nums2已经全部排除
+            return nums1[i + k - 1];
+        }
+        if (k == 1) {
+            return Math.min(nums1[i], nums2[j]);
+        }
+        // 比较i和j后面的k/2个数，
+        int midVal1 = (i + k / 2 - 1 < nums1.length) ? nums1[i + k / 2 - 1] : Integer.MAX_VALUE;
+        int midVal2 = (j + k / 2 - 1 < nums2.length) ? nums2[j + k / 2 - 1] : Integer.MAX_VALUE;
+        if (midVal1 < midVal2) {
+            return findKth(nums1, i + k / 2, nums2, j, k - k / 2);
+        } else {
+            return findKth(nums1, i, nums2, j + k / 2, k - k / 2);
+        }
+    }
+}
