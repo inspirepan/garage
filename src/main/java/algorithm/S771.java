d20ce8b763a4670899626de9566a8c318edd49bb
PanJx <panjx6@outlook.com>
2020-10-04 22:50:40 +0800

    771

---
+++ src/main/java/algorithm/S771.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+/**
+ * 太久没写题又忘记了，对于char类型的计数不要用Map、Set，用数组
+ */
+public class S771 {
+    public int numJewelsInStones(String J, String S) {
+        int[] record = new int[58];
+        if (J.length() == 0 || S.length() == 0) return 0;
+        for (char j : J.toCharArray()) record[j - 'A'] = 1;
+        int count = 0;
+        for (char s : S.toCharArray()) if (record[s - 'A'] == 1) count++;
+        return count;
+    }
+}
