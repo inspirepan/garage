2735a94335d976d65853fcedd82c4cf912f516fe
PanJx <panjx6@outlook.com>
2020-10-11 19:56:15 +0800

    268,416

--- src/main/java/algorithm/S416.java
+++ src/main/java/algorithm/S416.java
@@ -3,16 +3,12 @@ package algorithm;
 public class S416 {
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for (int num : nums) {
-            sum += num;
-        }
-        if (sum == 0) {
-            return true;
-        }
-        if ((sum & 1) != 0) {
-            return false;
-        }
+        for (int num : nums) sum += num;
+        if (sum == 0) return true;
+        if ((sum & 1) != 0) return false;
         int target = sum >> 1;
+        // 能否取出任意个数使得和等于target
+        // dp[i]大于0表示可以被表示为数组中的任意个数之和
         int[] dp = new int[target + 1];
         dp[0] = 1;
         for (int num : nums) {
