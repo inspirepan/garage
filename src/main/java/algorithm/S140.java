abee73de1a6b06eb0f8f75a361d3b582739448f5
PanJx <panjx6@outlook.com>
2020-10-12 14:14:34 +0800

    format 140

--- src/main/java/algorithm/S140.java
+++ src/main/java/algorithm/S140.java
@@ -1,11 +1,6 @@
 package algorithm;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.lang.StringBuilder;
+import java.util.*;
 
 public class S140 {
     private HashSet<String> wordDictSet;
@@ -17,27 +12,16 @@ public class S140 {
     }
 
     private List<String> wordBreakHelper(String s) {
-        if (map.containsKey(s)) {
-            return map.get(s);
-        }
+        if (map.containsKey(s)) return map.get(s);
         List<String> totalList = new ArrayList<>();
-        if (wordDictSet.contains(s)) {
-            totalList.add(s);
-            /* 不能直接返回<=>因为即便存在于wordDict中，也可能是其他两个word的组合 */
-            /*
-             * map.put(s, total); return total;
-             */
-        }
+        if (wordDictSet.contains(s)) totalList.add(s);
         for (int i = 0; i < s.length(); i++) {
             String sub = s.substring(i);
-            if (!wordDictSet.contains(sub)) {
-                continue;
-            }
+            if (!wordDictSet.contains(sub)) continue;
             List<String> leftList = wordBreakHelper(s.substring(0, i));
             List<String> combinedList = new ArrayList<>();
-            leftList.forEach(leftStr -> combinedList
-                    .add(new StringBuilder().append(leftStr).append(" ").append(sub).toString()));
-            combinedList.forEach(ss -> totalList.add(ss));
+            leftList.forEach(leftStr -> combinedList.add(leftStr + " " + sub));
+            totalList.addAll(combinedList);
         }
         map.put(s, totalList);
         return totalList;
