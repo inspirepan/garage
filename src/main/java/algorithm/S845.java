84974d636f83aafeaf3d311d8f883bdecb9763f9
PanJx <panjx6@outlook.com>
2020-10-25 15:27:54 +0800

    845

---
+++ src/main/java/algorithm/S845.java
@@ -0,0 +1,45 @@
+package algorithm;
+
+public class S845 {
+    public static void main(String[] args) {
+        System.out.println(new S845().longestMountain(new int[]{5770,9921,1853,441,6808,8847,9186,3091,1524,2415}));
+    }
+
+    public int longestMountain(int[] A) {
+        if (A.length < 3) return 0;
+        int start = 0;
+        int prev = A[0];
+        int max = 0;
+        boolean up = true;
+        for (int i = 1; i < A.length; i++) {
+            int curr = A[i];
+            // 上升
+            if (up) {
+                if (curr == prev) {
+                    start = i;
+                } else if (curr < prev) {
+                    if (i != start + 1) {
+                        up = false;
+                        max = Math.max(max, i - start + 1);
+                    } else {
+                        start = i;
+                    }
+                }
+            }
+            // 下降
+            else {
+                if (curr < prev) {
+                    max = Math.max(max, i - start + 1);
+                } else if (curr == prev) {
+                    start = i;
+                    up = true;
+                } else {
+                    start = i - 1;
+                    up = true;
+                }
+            }
+            prev = curr;
+        }
+        return max;
+    }
+}
