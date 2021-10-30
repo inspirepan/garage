6013f5dbfe988698ac24678d5c28bb69ffd023fe
panjx <panjx6@outlook.com>
2021-10-30 13:49:14 +0800

    30

--- src/main/java/algorithm/S77.java
+++ src/main/java/algorithm/S77.java
@@ -4,23 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class S77 {
-    private final List<List<Integer>> result = new ArrayList<>();
-    private final List<Integer> path = new ArrayList<>();
+    public List<List<Integer>> result = new ArrayList<>();
+    public List<Integer> path = new ArrayList<>();
 
     public List<List<Integer>> combine(int n, int k) {
         dfs(n, k, 1, 0);
         return result;
     }
 
-    private void dfs(int n, int k, int s, int len) {
+    void dfs(int n, int k, int start, int len) {
         if (len == k) {
             result.add(new ArrayList<>(path));
             return;
         }
-        int left = k - len;
-        for (int i = s + 1; i <= n - left + 1; i++) {
+        for (int i = start; i <= n - k + len + 1; i++) {
             path.add(i);
-            dfs(n, k, i, len + 1);
+            dfs(n, k, i + 1, len + 1);
             path.remove(len);
         }
     }
---
+++ src/test/java/algorithm/S73.java
@@ -0,0 +1,61 @@
+package algorithm;
+
+public class S73 {
+    public void setZeroes(int[][] matrix) {
+        // use the first col and row to record
+        int m = matrix.length;
+        if (m == 0) {
+            return;
+        }
+        int n = matrix[0].length;
+        if (n == 0) {
+            return;
+        }
+        boolean first_row = false;
+        boolean first_col = false;
+        for (int[] ints : matrix) {
+            if (ints[0] == 0) {
+                first_row = true;
+                break;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (matrix[0][i] == 0) {
+                first_col = true;
+                break;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < n; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 0; i < m; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (first_row) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        if (first_col) {
+            for (int i = 0; i < n; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+    }
+}
