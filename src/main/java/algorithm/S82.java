8c7284ab5c25ca7cf4d5f3c3a0e619f8aecc6fd8
PanJx@arch <panjx6@outlook.com>
2021-11-01 16:40:24 +0800

    82

--- src/main/java/algorithm/S82.java
+++ src/main/java/algorithm/S82.java
@@ -3,43 +3,32 @@ package algorithm;
 import datastructure.ListNode;
 
 public class S82 {
+    /**
+     * a conciser version
+     */
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
-        ListNode dummyHead = new ListNode(0);
-        dummyHead.next = head;
-        // q 用于连接下一新数值
-        // p 表示当前正在判断的结点
-        ListNode q = dummyHead;
+        ListNode dummyHead = new ListNode();
         ListNode p = head;
-        int currValue = head.val;
+        ListNode q = dummyHead;
+        dummyHead.next = head;
+
         while (p.next != null) {
-            int nextValue = p.next.val;
-            if (nextValue != currValue) {
-                // 当前结点不重复，接上
-                q.next = p;
+            if (p.next.val != p.val) {
                 q = q.next;
-                currValue = nextValue;
             } else {
-                // 重复，移动到下一个新结点
-                while (p.next != null && p.next.val == currValue) {
+                while (p.next != null && p.next.val == p.val) {
                     p = p.next;
                 }
-                if (p.next != null) {
-                    // 修改比较用的数值currValue
-                    currValue = p.next.val;
-                } else {
-                    // 移动到了最后，直接返回结果
-                    q.next = null;
+                q.next = p.next;
+                if (p.next == null) {
                     return dummyHead.next;
                 }
             }
             p = p.next;
         }
-        // 补上最后一个结点
-        q.next = p;
-        q.next.next = null;
         return dummyHead.next;
     }
 }
