1116c6c9cc72231fdd0bbbbcfcda5c8153475aff
PanJx <narfnas@outlook.com>
2022-02-24 12:17:43 +0800

    2.23

--- src/main/java/algorithm/C1/S174.java
+++ src/main/java/algorithm/C1/S174.java
@@ -5,20 +5,22 @@ package algorithm.C1;
  */
 public class S174 {
     public int calculateMinimumHp(int[][] dungeon) {
-        int row = dungeon.length;
-        int col = dungeon[0].length;
-        int[][] dp = new int[row][col];
+        int m = dungeon.length;
+        int n = dungeon[0].length;
+        int[][] dp = new int[m][n];
         // 最后一间
-        dp[row - 1][col - 1] = dungeon[row - 1][col - 1] > 0 ? 1 : 1 - dungeon[row - 1][col - 1];
-        for (int i = row - 2; i >= 0; i--) {
+        dp[m - 1][n - 1] = dungeon[m - 1][n - 1] > 0 ? 1 : 1 - dungeon[m - 1][n - 1];
+        // 最后一列
+        for (int i = m - 2; i >= 0; i--) {
             // 最低也需要生命值为1
-            dp[i][col - 1] = Math.max(dp[i + 1][col - 1] - dungeon[i][col - 1], 1);
+            dp[i][n - 1] = Math.max(dp[i + 1][n - 1] - dungeon[i][n - 1], 1);
         }
-        for (int j = col - 2; j >= 0; j--) {
-            dp[row - 1][j] = Math.max(dp[row - 1][j + 1] - dungeon[row - 1][j], 1);
+        // 最后一行
+        for (int j = n - 2; j >= 0; j--) {
+            dp[m - 1][j] = Math.max(dp[m - 1][j + 1] - dungeon[m - 1][j], 1);
         }
-        for (int i = row - 2; i >= 0; i--) {
-            for (int j = col - 2; j >= 0; j--) {
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
                 // 计算最低需求用min，但是也不能低于1
                 dp[i][j] = Math.max(-dungeon[i][j] + Math.min(dp[i + 1][j], dp[i][j + 1]), 1);
             }
--- src/main/java/algorithm/S491.java
+++ src/main/java/algorithm/S491.java
@@ -4,29 +4,33 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class S491 {
+
     List<List<Integer>> result = new LinkedList<>();
     LinkedList<Integer> path = new LinkedList<>();
 
     public List<List<Integer>> findSubsequences(int[] nums) {
+        // 题目要求的不是连续子序列，是子序列！！
         dfs(0, Integer.MIN_VALUE, nums);
         return result;
     }
 
     private void dfs(int start, int last, int[] nums) {
+        // 到终点了
         if (start == nums.length) {
             if (path.size() > 1) {
                 result.add(new LinkedList<>(path));
             }
             return;
         }
+        // 如果递增
         if (nums[start] >= last) {
             path.add(nums[start]);
             dfs(start + 1, nums[start], nums);
             path.removeLast();
         }
+        // 如果一样，剪枝；path中不加入当前节点
         if (nums[start] != last) {
             dfs(start + 1, last, nums);
         }
     }
-
 }
