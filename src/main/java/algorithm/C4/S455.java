a7570e1f0408e90b79f4548f4b3b697486a65f69
PanJx <narfnas@outlook.com>
2022-03-07 14:08:32 +0800

    3.7

---
+++ src/main/java/algorithm/C4/S455.java
@@ -0,0 +1,21 @@
+package algorithm.C4;
+
+import java.util.Arrays;
+
+public class S455 {
+    public int findContentChildren(int[] g, int[] s) {
+        // 排序全部的饼干和胃口
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        int j = 0;
+        while (i < g.length && i + j < s.length) {
+            if (g[i] > s[i + j]) {
+                while (i + j < s.length && g[i] > s[i + j]) {
+                    j++;
+                }
+            } else i++;
+        }
+        return i;
+    }
+}
